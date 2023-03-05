6c32c1d1da820d5e1b3971886a4fdde00edf1a64
OluwafemiAjiboye <ajiboyefemi15@gmail.com>
2023-03-05 18:07:10 +0000

    created a new flutter project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_flutter/screens/splash_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF1D1D28),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
